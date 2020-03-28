6edbde12d09e6d3a9c33067f6dbd981c67294025
Jochen Seidel <jochen.seidel@gmail.com>
2020-03-28 13:42:26 +0100

    add hashing of preimage to obtain actual token value, adapt to db schema change (renaming of column token -> token_value)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sqlalchemy
 from flask import jsonify
 from base64 import b64encode, b64decode
 from binascii import Error as BinasciiError
+import hashlib
 
 TOKEN_BYTES = 16
 
@@ -21,45 +22,53 @@ def db_connect():
 		)
 	).connect()
 
+def virustracker_hash(preimage):
+	return hashlib.sha256(b"VIRUSTRACKER"+preimage)
+
 def tokens(request):
 	if request.method == 'GET':
 		with db_connect() as conn:
-			all_tokens = conn.execute('SELECT token, report_type FROM token WHERE report_result = "POSITIVE"').fetchall()
+			all_tokens = conn.execute("SELECT token_value, report_type FROM token WHERE report_result = 'POSITIVE'").fetchall()
 			return jsonify({'tokens': [{'value': b64encode(row[0]).decode('ascii'), 'type': row[1]} for row in all_tokens]})
 	elif request.method == 'POST':
 		# Validate input
 		try:
 			request_json = request.get_json()
 			if not request_json or not isinstance(request_json, dict) or 'report' not in request_json:
-				raise BadInputException()
+				raise BadInputException("Request must contain JSON, and must have a report.")
 			report = request_json['report']
 			if not isinstance(report, dict) or not all(field in report for field in ('tokens', 'type', 'result')):
-				raise BadInputException()
+				raise BadInputException("Report must be a dict, and must contain 'tokens', 'type', and 'result'.")
 			tokens = report['tokens']
 			type   = report['type']
 			result = report['result']
 			if not isinstance(tokens, list):
-				raise BadInputException()
+				raise BadInputException("Tokens must be a list.")
 			for token in tokens:
-				if not isinstance(token, dict) or 'value' not in token or not isinstance(token['value'], str):
-					raise BadInputException()
-				if len(token['value']) != math.ceil(TOKEN_BYTES / 3) * 4 or len(b64decode(token['value'], validate=True)) != TOKEN_BYTES:
-					raise BadInputException()
+				if not isinstance(token, dict) or 'preimage' not in token or not isinstance(token['preimage'], str):
+					raise BadInputException("POSTed tokens must have a preimage.")
+				if len(token['preimage']) != math.ceil(TOKEN_BYTES / 3) * 4 or len(b64decode(token['preimage'], validate=True)) != TOKEN_BYTES:
+					raise BadInputException("Could not parse token preimage.")
 				if any(latlong in token and not isinstance(token[latlong], (float, int)) for latlong in ('lat', 'long')):
-					raise BadInputException()
+					raise BadInputException("Could not parse token location.")
 			if not isinstance(type, str) or type not in ('SELF_REPORT', 'VERIFIED'):
-				raise BadInputException()
+				raise BadInputException("Could not parse token type.")
 			if not isinstance(result, str) or result not in ('UNKNOWN', 'POSITIVE', 'NEGATIVE'):
-				raise BadInputException()
-		except (BadInputException, BinasciiError):
-			return ('Bad Request', 400)
+				raise BadInputException("Could not parse token result.")
+		except (BadInputException, BinasciiError) as err:
+			return ('Bad Request: '+err, 400)
 		
 		# Commit input to database
-		stmt = sqlalchemy.text('INSERT INTO token (token, report_type, report_result, location_lat, location_long) VALUES (:token, :report_type, :report_result, :location_lat, :location_long) ON CONFLICT (token) DO UPDATE SET report_type = :report_type, report_result = :report_result, location_lat = :location_lat, location_long = :location_long')
+		stmt = sqlalchemy.text(
+			'INSERT INTO token (token_value, report_type, report_result, location_lat, location_long) '
+			'VALUES (:token_value, :report_type, :report_result, :location_lat, :location_long) '
+			'ON CONFLICT (token_value) DO UPDATE '
+			'SET report_type = :report_type, report_result = :report_result, location_lat = :location_lat, '
+			'location_long = :location_long')
 		with db_connect() as conn:
 			for token in tokens:
 				new_row = {}
-				new_row['token'] = b64decode(token['value'])
+				new_row['token_value'] = virustracker_hash(b64decode(token['preimage'])).digest()
 				new_row['report_type'] = type
 				new_row['report_result'] = result
 				if 'lat' in token and 'long' in token:
@@ -73,3 +82,4 @@ def tokens(request):
 		return 'Success!'
 	else:
 		return ('Method Not Allowed', 405)
+
