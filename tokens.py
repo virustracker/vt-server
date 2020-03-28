748cc352bce666f694d6fc7f966771e4153a4636
Christian Decker <decker.christian@gmail.com>
2020-03-28 14:02:29 +0100

    tokens: Make `generate` a class method of Token

--- tokens.py
+++ tokens.py
@@ -2,13 +2,22 @@ import hashlib
 from binascii import hexlify, unhexlify
 from collections import namedtuple
 import secrets
+from typing import List, Type, TypeVar
 
+
+T = TypeVar('T', bound='Parent')
 token_len = 32
 salt = b"virustracker"
 rotate_interval = 5*60  # Rotate token every 5 minutes
 
 
 class Token(object):
+    """A locally generated token, or from a device when verifying.
+
+    The token consists of a the tuple `(preimage, hash, timeslot)` and is
+    regenerated every `rotate_interval` seconds.
+
+    """
     def __init__(self, preimage: bytes, slot: int) -> None:
         assert(len(preimage) == 32)
         self.preimage = preimage
@@ -23,8 +32,11 @@ class Token(object):
     def timestamp(self) -> int:
         return self.slot * rotate_interval
 
+    @classmethod
+    def generate(cls: Type[T], timestamp: int) -> T:
+        preimage = secrets.token_bytes(32)
+        slot = timestamp // rotate_interval
+        return Token(preimage, slot)
 
-def generate(timestamp: int) -> Token:
-    preimage = secrets.token_bytes(32)
-    slot = timestamp // rotate_interval
-    return Token(preimage, slot)
+
+
