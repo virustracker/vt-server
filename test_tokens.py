8e4916fa5990cb885377dbd3c6c4a01e0ed9ac91
Christian Decker <decker.christian@gmail.com>
2020-03-28 14:03:23 +0100

    tokens: Add a class for the Request sent from the client to the server
    This class encapsulates the request generation logic from the tokens to the
    Request and then verifying that the request matches when the client supplies
    the tokens in response to a test result.

--- test_tokens.py
+++ test_tokens.py
@@ -1,4 +1,4 @@
-from tokens import generate, Token
+from tokens import Token, Request
 
 
 def test_generate():
@@ -9,3 +9,17 @@ def test_generate():
     # Tokens are not timedependent
     t2 = Token(b'A'*32, 300)
     assert(t.hash() == t2.hash() and t.slot != t2.slot)
+
+
+def test_request():
+    tokens = [Token(bytes([i])*32, i*300) for i in range(0, 256)]
+    req = Request.create(tokens)
+
+    assert(req.hash == "3261e3b141d03594b31d5ae7d64eca31ee664b8095c64fc04fb8bca0466d6b09")
+    assert(req.verify([t.preimage for t in tokens]))
+
+    # Omitting any secret should not work
+    for i in range(0, 255):
+        assert(not req.verify([t.preimage for t in tokens[i:i+1]]))
+
+    assert(req.hash.startswith(req.id()))
--- tokens.py
+++ tokens.py
@@ -9,6 +9,7 @@ T = TypeVar('T', bound='Parent')
 token_len = 32
 salt = b"virustracker"
 rotate_interval = 5*60  # Rotate token every 5 minutes
+id_len = 8
 
 
 class Token(object):
@@ -38,5 +39,40 @@ class Token(object):
         slot = timestamp // rotate_interval
         return Token(preimage, slot)
 
+
+
+class Request(object):
+    """A hash of multiple concatenated preimages used to identify the test instance.
+
+    The client device generates the request from a set of tokens, then passes
+    the hash to the server. Later the client also supplies the preimages and
+    the server can verify that the preimages match its expectation.
+
+    """
+
+    def __init__(self, hash) -> None:
+        self.hash = hash
+
+    def id(self) -> str:
+        return self.hash[:id_len]
+
+    def verify(self, preimages) -> bool:
+        m = hashlib.sha256()
+        m.update(salt)
+
+        for p in preimages:
+            m.update(p)
+
+        return self.hash == m.hexdigest()
+
+    @classmethod
+    def create(cls: Type[T], tokens: List[Token]) -> T:
+        m = hashlib.sha256()
+        m.update(salt)
+
+        for t in tokens:
+            m.update(t.preimage)
+
+        return Request(m.hexdigest())
 
 
