6007afcb7b60ea07b3b280e500286494ebb62036
mikonig <62788747+mikonig@users.noreply.github.com>
2020-03-30 05:22:07 +0200

    Added verification for verified report submissions.

--- dbschema.sql
+++ dbschema.sql
@@ -1,7 +1,16 @@
 CREATE TYPE report_type AS ENUM ('SELF_REPORT', 'VERIFIED');
 CREATE TYPE report_result AS ENUM ('UNKNOWN', 'POSITIVE', 'NEGATIVE');
 
-CREATE TABLE token (token bytea PRIMARY KEY, location_lat double precision NULL, location_long double precision NULL, report_type report_type NOT NULL, report_result report_result NOT NULL);
-
+CREATE TABLE token (token_value bytea PRIMARY KEY, report_type report_type NOT NULL, report_result report_result NOT NULL);
 COMMENT ON TABLE token is 'Contains one row per uploaded token. If a token is submitted in multiple reports, its row reflects the latest report.';
-COMMENT ON COLUMN token.token is 'The token as transmitted via Bluetooth. Always 16 bytes.';
+COMMENT ON COLUMN token.token_value is 'The token value as transmitted via Bluetooth. Always 32 bytes.';
+
+CREATE TABLE certificate (attestation_hash_prefix bytea PRIMARY KEY, result report_result NOT NULL, lab_id bigint NOT NULL);
+COMMENT ON TABLE certificate is 'Submitted attestation certificates.';
+COMMENT ON COLUMN certificate.attestation_hash_prefix is 'The hash prefix as given to lab personnel. At least 8 bytes.';
+COMMENT ON COLUMN certificate.lab_id is '(always positive)';
+
+CREATE TABLE lab (id bigint PRIMARY KEY, key bytea NOT NULL);
+COMMENT ON TABLE lab is 'Registered labs and their HMAC keys.';
+COMMENT ON COLUMN lab.id is '(always positive)';
+COMMENT ON COLUMN lab.key is 'The HMAC key this lab uses in its attestations.';
--- server/main.py
+++ server/main.py
@@ -5,6 +5,7 @@ from flask import jsonify
 from base64 import b64encode, b64decode
 from binascii import Error as BinasciiError
 import hashlib
+import hmac
 
 PREIMAGE_BYTES = 32
 TOKEN_VALUE_BYTES = 32
@@ -13,6 +14,9 @@ def compute_token_value(preimage):
   return hashlib.sha256(b"VIRUSTRACKER" + preimage).digest()
 
 class BadInputException(Exception):
+  pass
+
+class ForbiddenException(Exception):
   pass
 
 db = sqlalchemy.create_engine(
@@ -33,7 +37,7 @@ def db_execute(conn, stmt, values):
   conn.execute(stmt, **values)
 
 # Add to or update token table
-def store_tokens(tokens, report_result, report_type):
+def store_tokens(token_values, report_result, report_type):
   # Disallow overwriting verified results with self-reported results.
   update_condition = ' WHERE report_type <> "VERIFIED"' if report_type != 'VERIFIED' else ''
   
@@ -46,36 +50,67 @@ def store_tokens(tokens, report_result, report_type):
   )
   
   with db_connect() as conn:
-    for token in tokens:
-      new_row = {}
-      new_row['token_value'] = compute_token_value(b64decode(token['preimage']))
-      new_row['report_type'] = report_type
-      new_row['report_result'] = report_result
+    new_row = {}
+    new_row['report_type'  ] = report_type
+    new_row['report_result'] = report_result
+    for token_value in token_values:
+      new_row['token_value'] = token_value
       db_execute(conn, stmt, new_row)
 
 def process_report(report):
   # Validate input
-  if not isinstance(report, dict) or not all(field in report for field in ('tokens', 'type', 'result')):
-    raise BadInputException("Report must be a dict, and must contain 'tokens', 'type', and 'result'.")
+  if not isinstance(report, dict) or not all(field in report for field in ('tokens', 'type')):
+    raise BadInputException("Report must be a dict, and must contain 'tokens', and 'type'.")
   tokens = report['tokens']
   report_type = report['type']
-  report_result = report['result']
   if not isinstance(tokens, list):
     raise BadInputException("Tokens must be a list.")
   for token in tokens:
     if not isinstance(token, dict) or 'preimage' not in token or not isinstance(token['preimage'], str):
-      raise BadInputException("POSTed tokens must have a preimage.")
+      raise BadInputException("Submitted tokens must have a preimage.")
     try:
       if len(token['preimage']) != math.ceil(PREIMAGE_BYTES / 3) * 4 or len(b64decode(token['preimage'], validate=True)) != PREIMAGE_BYTES:
         raise BadInputException("Could not parse token preimage.")
     except (BinasciiError, ValueError) as err:
       raise BadInputException("Invalid Base64.")
+  preimages = [b64decode(token['preimage']) for token in tokens]
   if not isinstance(report_type, str) or report_type not in ('SELF_REPORT', 'VERIFIED'):
     raise BadInputException("Could not parse token type.")
-  if not isinstance(report_result, str) or report_result not in ('UNKNOWN', 'POSITIVE', 'NEGATIVE'):
-    raise BadInputException("Could not parse token result.")
+  if report_type == 'SELF_REPORT':
+    if 'result' not in report:
+      raise BadInputException("Self-reports must specify 'result'.")
+    report_result = report['result']
+    if not isinstance(report_result, str) or report_result not in ('UNKNOWN', 'POSITIVE', 'NEGATIVE'):
+      raise BadInputException("Could not parse token result.")
+  elif report_type == 'VERIFIED':
+    if 'result' in report:
+      raise BadInputException("Verified reports cannot specify 'result'.")
+    if 'attestation_hash_prefix' not in report:
+      raise BadInputException("Verified reports must specify 'attestation_hash_prefix'.")
+    try:
+      ahp = b64decode(report['attestation_hash_prefix'], validate=True)
+    except (BinasciiError, ValueError) as err:
+      raise BadInputException("Invalid Base64.")
+    if len(ahp) < 8:
+      raise BadInputException("attestation_hash_prefix too short.")
+    
+    # Verify attestation_hash_prefix is correct and has an attestation
+    if not verify_ahp(preimages, ahp):
+      raise ForbiddenException()
+    with db_connect() as conn:
+      rows = conn.execute(sqlalchemy.text('SELECT report_result FROM certificate WHERE attestation_hash_prefix = :ahp'), {'ahp': ahp}).fetchall()
+      if not rows:
+        raise ForbiddenException()
+      report_result = rows[0][0]
+  assert report_result
   
-  store_tokens(tokens, report_result, report_type)
+  store_tokens(compute_token_value(pi) for pi in preimages, report_result, report_type)
+
+def verify_ahp(preimages, ahp):
+  m = hashlib.sha256()
+  for pi in preimages:
+    m.update(pi)
+  return hmac.compare_digest(ahp, m.digest()[:len(ahp)])
 
 def tokens(request):
   if request.method == 'GET':
@@ -88,11 +123,11 @@ def tokens(request):
       if not request_json or not isinstance(request_json, dict) or 'report' not in request_json:
         raise BadInputException("Request must contain JSON, and must have a report.")
       report = request_json['report']
+      process_report(report)
     except (BadInputException) as err:
       return ('Bad Request: ' + str(err), 400)
-    
-    process_report(report)
-    
+    except (ForbiddenException):
+      return ('Forbidden', 403)
     return 'Success!'
   else:
     return ('Method Not Allowed', 405)
